a216b94c997f448da1d00f7240ce48f5d67a6d2b
ATeshome <71078949+AlemuTeshome@users.noreply.github.com>
2023-11-07 01:27:40 -0600

    lab6

---
+++ lab6/PrintNumbers.js
@@ -0,0 +1,13 @@
+function printNumbers(from, to) {
+    let currentNumber = from;
+    const intervalId = setInterval(function () {
+      console.log(currentNumber);
+      if (currentNumber === to) {
+        clearInterval(intervalId);
+      }
+      currentNumber++;
+    }, 1000);
+  }
+  printNumbers(1, 5);
+
+  
